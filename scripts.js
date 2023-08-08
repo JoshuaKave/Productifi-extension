a02fc94ab58163d15aacacdf93003c3e6b2890bf
JoshuaKave <jmkav192@gmail.com>
2023-08-07 21:22:33 -0700

    links now redirect when blocked

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
 const inputEl = document.getElementById("input-url");
+const blkBtnEl = document.getElementById("blk-btn");
 
 const blockedURLS = [];
 
-function block(){
+blkBtnEl.addEventListener("click", myFunction);
+
+function myFunction(){
+    //pushes input value to blocked urls array
     blockedURLS.push(inputEl.value);
-    console.log(blockedURLS);
+    //console.log(blockedURLS);
+    //updates localstorage to the most recent array
+    localStorage.setItem("blocked", JSON.stringify(blockedURLS));  
+    //console.log("hello");
+};
+
+//gets current tab info
+function handleUpdated(tabId, changeInfo, tab){
+  const blockedUrlsFetch = JSON.parse(localStorage.getItem("blocked"));
+  for(let i = 0; i < blockedUrlsFetch.length; i++){  
+    if(tab.url.includes(blockedUrlsFetch[i])){
+      let updating = chrome.tabs.update({url: "https://youtube.com"});
+    }
+  }
 }
+
+chrome.tabs.onUpdated.addListener(handleUpdated);
