87837d81a62c47266121f4c88b213df351507122
JoshuaKave <jmkav192@gmail.com>
2023-10-18 18:55:26 -0700

    styled new form, icons

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,46 @@
 const inputEl = document.getElementById("input-url");
 const saveBtn = document.getElementById("save-btn");
 const saveList = document.getElementById("save-list");
-const localArray = JSON.parse(localStorage.getItem("saved-urls"));
 const clearBtn = document.getElementById("clear-btn");
+const inputName = document.getElementById("input-name");
+const inputDate = document.getElementById("input-date");
+const saveForm = document.getElementById("save-form");
+const saveUrlBtn = document.getElementById("save-url");
 
 let saveArray = [];
+const localArray = JSON.parse(localStorage.getItem("saved-urls"));
 
 if (localArray) {
     saveArray = localArray;
     display(saveArray);
-    console.log(saveArray.length);
 }
 
+
 saveBtn.addEventListener("click", function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        saveArray.push(tabs[0].url);
-        localStorage.setItem("saved-urls", JSON.stringify(saveArray));
-        display(saveArray);
-    })
+    saveForm.style.display = "block";
 });
 
-clearBtn.addEventListener("click", function(){
+saveUrlBtn.addEventListener("click", function(){
+    const name = inputName.value;
+    const date = inputDate.value;
+    if(name && date){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            const url = tabs[0].url;
+            const data = {name, date, url};
+            saveArray.push(data);
+            localStorage.setItem("saved-urls", JSON.stringify(saveArray));
+            display(saveArray);
+        })
+        inputName.value = "";
+        inputDate.value = "";   
+        saveForm.style.display="none";
+    }
+    else{
+        saveForm.style.display="none";
+    }
+});
+
+clearBtn.addEventListener("dblclick", function(){
     localStorage.clear();
     saveArray = [];
     saveList.innerHTML = "";
@@ -30,10 +50,11 @@ clearBtn.addEventListener("click", function(){
 function display(links){
     let saved = "";
     for(let i = 0; i < links.length; i++){
+        const data = links[i];
         saved += `
         <li>
-            <a target='_blank' href='${links[i]}'>
-                ${links[i]}
+            <a target='_blank' href='${data.url}'>
+                ${data.name} Due: ${data.date}
             </a>
         </li>
     `
